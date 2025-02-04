dc0ae7accdf164edffadaca5b4d75f95ffe65d92
giv3x <giv3xconnect@gmail.com>
2025-02-04 20:34:19 +0700

    eight practicalwork

--- js/main.js
+++ js/main.js
@@ -1,29 +1,65 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand:'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
+Vue.component('product',{
+    template: `  
+  <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText">
+        </div>
 
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
-            }
-         ],
-         cart: 0,
-         onSale:true,
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In stock</p>
+            <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+             <product-details :details="details"></product-details>
+                    <p>Shipping: {{ shipping }}</p>
 
+            <div  class="color-box"
+                  v-for="(variant, index) in variants"
+                  :key="variant.variantId"
+                  :style="{ backgroundColor:variant.variantColor }"
+                  @mouseover="updateProduct(index)"
+            >
+            </div>
+
+            <div class="cart">
+                <p>Cart({{ cart}})</p>
+            </div>
+            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to Cart</button>
+
+        </div>
+    </div>`,
+
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10
+
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0,
+            onSale: true,
+        }
+    },
+
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
     },
 
     methods:{
@@ -33,8 +69,8 @@ let app = new Vue({
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
-         }
-         
+        }
+
     },
 
     computed:{
@@ -46,12 +82,38 @@ let app = new Vue({
         },
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
-         },
+        },
         sale(){
             if(this.onSale){
                 return this.brand + ' ' + this.product + 'are on sale!'
             }
-                return this.brand + ' ' + this.product + 'are not one sale!'
+            return this.brand + ' ' + this.product + 'are not one sale!'
+        },
+        shipping(){
+            if (this.premium) {
+                return "Free"
+            } else {
+               return 2.99
             }
-         }
+        }
+    }
+})
+
+Vue.component('product-details',{
+    template: ` <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>`,
+    props: {
+        details:{
+            type: Array,
+            required:true
+        }
+    }
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
 })
